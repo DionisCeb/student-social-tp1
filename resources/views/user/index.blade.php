81e21cfc5a09865b79e6681734b9ba3bea4b3ef6
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-11-07 19:34:08 -0500

    custom pagination

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Users')
 @section('content')
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 <div class="container">
     <table class="table">
         <thead>
@@ -22,7 +22,42 @@
             @endforeach
         </tbody>
     </table>
-    {{ $users }}
+   <!-- Custom Pagination -->
+   <div class="pagination-container mt-4">
+        <ul class="pagination flex justify-center">
+            {{-- Previous Page Link --}}
+            @if ($users->onFirstPage())
+                <li class="disabled">
+                    <span>&laquo; Previous</span>
+                </li>
+            @else
+                <li>
+                    <a href="{{ $users->previousPageUrl() }}" class="text-gray-700 hover:text-gray-900">&laquo; Previous</a>
+                </li>
+            @endif
+
+            {{-- Page Indexes --}}
+            @foreach ($users->links()->elements[0] as $page => $url)
+                {{-- Skip "first" and "last" buttons --}}
+                @if (is_numeric($page))
+                    <li class="{{ $page == $users->currentPage() ? 'active' : '' }}">
+                        <a href="{{ $url }}" class="py-1 px-3 text-gray-700 hover:text-gray-900 {{ $page == $users->currentPage() ? 'bg-blue-500 text-white' : '' }}">{{ $page }}</a>
+                    </li>
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($users->hasMorePages())
+                <li>
+                    <a href="{{ $users->nextPageUrl() }}" class="text-gray-700 hover:text-gray-900">Next &raquo;</a>
+                </li>
+            @else
+                <li class="disabled">
+                    <span>Next &raquo;</span>
+                </li>
+            @endif
+        </ul>
+    </div>
 </div>
 
 
