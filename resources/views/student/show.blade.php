0b4fc2559bcf4a9b218849d57a23069316f29e82
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-11-14 11:36:50 -0500

    personal profile

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -6,25 +6,27 @@
 <div class="profile">
     <div class="profile-image">
         <img src="{{ asset('img/profile/profile.webp')}}" alt="profile-image">
-        <div class="profile-links flex-col gap20 mt-20">
-            <a href="{{ route('student.edit', $student->id) }}" class="btn btn-icon">Modifier <i class="fa-solid fa-pen"></i></a>
-            <!-- Delete Button -->
-            <button class="btn btn-icon danger" id="open-popup">Supprimer <i class="fa-solid fa-trash"></i></button>
+        @if(Auth::check() && Auth::id() === $student->user_id)
+            <div class="profile-links flex-col gap20 mt-20">
+                <a href="{{ route('student.edit', $student->id) }}" class="btn btn-icon">Modifier <i class="fa-solid fa-pen"></i></a>
+                <!-- Delete Button -->
+                <button class="btn btn-icon danger" id="open-popup">Supprimer <i class="fa-solid fa-trash"></i></button>
 
-            <div id="popup" class="popup">
-                <div class="popup-content">
-                    <h3>Etes-vous sûr de vouloir supprimer cet élève ?</h3>
-                    <form action="{{ route('student.destroy', $student->id) }}" method="POST" class="flex-col gap20">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-icon danger">
-                            Supprimer <i class="fa-solid fa-trash"></i>
-                        </button>
-                        <button type="button" class="btn" id="close-popup">Annuler</button>
-                    </form>
+                <div id="popup" class="popup">
+                    <div class="popup-content">
+                        <h3>Etes-vous sûr de vouloir supprimer cet élève ?</h3>
+                        <form action="{{ route('student.destroy', $student->id) }}" method="POST" class="flex-col gap20">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-icon danger">
+                                Supprimer <i class="fa-solid fa-trash"></i>
+                            </button>
+                            <button type="button" class="btn" id="close-popup">Annuler</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endif
     </div>
     <div class="profile-body">
         <h2 class="profile-name">{{ $student->name }}</h2>
